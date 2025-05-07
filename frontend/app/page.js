4b3168d3430ed2ef33f42b343e46f161dc97a4d4
Arshad Qamar <arshadqamar99@gmail.com>
2025-05-07 20:35:45 +0500

    added start/stop button

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -21,14 +21,27 @@ export default function Dashboard() {
       });
   }, []);
 
+  // Handle start
+  const handleStart = (channelId) =>{
+    console.log(`starting channel with id ${channelId}`)
+
+  }
+  //Handle stop
+  const handleStop = (channelId) =>{
+    console.log(`stopping channel with id ${channelId}`);
+    
+  }
+
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">📡 Channel Dashboard</h1>
-
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl font-bold">📡 Channel Dashboard</h1>
+        <button className="bg-orange-400 text-white px-2 py-1 rounded hover:bg-green-700">
+          + Channel
+        </button>
+      </div>
       {/* Show loading state */}
-      {loading ? (
-        <p>Loading channels...</p>
-      ) : (
+      {loading ? null : (
         <table className="w-full border-collapse border border-gray-300">
           <thead>
             <tr className="bg-gray-100">
@@ -36,15 +49,29 @@ export default function Dashboard() {
               <th className="border border-gray-300 px-4 py-2">Name</th>
               <th className="border border-gray-300 px-4 py-2">Input Type</th>
               <th className="border border-gray-300 px-4 py-2">Status</th>
+              <th className="border border-gray-300 px-4 py-2">Action</th>
             </tr>
           </thead>
           <tbody>
             {channels.map(channel => (
               <tr key={channel.id}>
-                <td className="border border-gray-300 px-4 py-2">{channel.id}</td>
-                <td className="border border-gray-300 px-4 py-2">{channel.name}</td>
-                <td className="border border-gray-300 px-4 py-2">{channel.input_type}</td>
-                <td className="border border-gray-300 px-4 py-2">{channel.status}</td>
+                <td className="border border-gray-300 px-4 py-1 text-center align-middle">{channel.id}</td>
+                <td className="border border-gray-300 px-4 py-1 text-center align-middle">{channel.name}</td>
+                <td className="border border-gray-300 px-4 py-1 text-center align-middle">{channel.input_type}</td>
+                <td className="border border-gray-300 px-4 py-1 text-center align-middle">{channel.status}</td>
+                <td className="border border-gray-300 px-4 py-1 text-center align-middle">
+                  {
+                    channel.status == "running" ?(
+                      <button className="bg-red-500 text-white py-1 px-3 rounded hover:bg-green-500"
+                      onClick={()=>handleStop(channel.id)}
+                      > Stop
+                      </button>
+                    ): <button className="bg-green-500 text-white py-1 px-3 rounded hover:bg-red-500"
+                    onClick={()=>{handleStart(channel.id)}}>
+                      Start
+                    </button>
+                  }
+                </td>
               </tr>
             ))}
           </tbody>

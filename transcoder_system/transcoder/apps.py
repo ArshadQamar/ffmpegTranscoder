da73e27d06aa7a96f4849a488aef78bcf4819e58
Arshad Qamar <arshadqamar99@gmail.com>
2025-07-07 16:27:41 +0500

    implemented auto channel restart in case of restart

--- transcoder_system/transcoder/apps.py
+++ transcoder_system/transcoder/apps.py
@@ -1,10 +1,22 @@
+import os
 from django.apps import AppConfig
-
 
 class TranscoderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'transcoder'
 
-    # Import signals so they get registered
     def ready(self):
         import transcoder.signals
+
+        # Only run this once in main process
+        if os.environ.get('RUN_MAIN') == 'true' and not os.environ.get('CHANNEL_RESTARTED'):
+            os.environ['CHANNEL_RESTARTED'] = '1'  # Prevent double init
+
+            from transcoder.models import TranscodingJob
+            from transcoder.tasks import transcoding_start
+
+            running_jobs = TranscodingJob.objects.filter(status='running')
+
+            for job in running_jobs:
+                print(f"Auto-restarting job: {job.channel.name}")
+                transcoding_start.delay(job.id)
